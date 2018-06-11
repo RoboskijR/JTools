2c622623ea2dd2581143d38b3a04a71b0ae637c7
Roboskijr <36843179+dhikaxrianz@users.noreply.github.com>
2018-06-12 05:39:19 +0700

    Add files via upload

---
+++ JTools.sh
@@ -0,0 +1,124 @@
+#!/system/xbin/bash
+clear
+echo -n "\033[33;1m "; date +"%d %B %Y"
+sleep 1
+echo " =================== Welcome To ===================";
+sleep 1
+toilet -f slant --gay JTools
+sleep 1
+echo "\033[33;1m ============= Coded By RoBoSkIjR_404 =============";
+sleep 1
+echo ""
+echo "\033[31;1m Tools  : Jones Tools"
+sleep 1
+echo "\033[32;1m Author : RoBoSkIjR_404"
+sleep 1
+echo "\033[31;1m Team   : Fox Cyber"
+sleep 1
+echo "\033[32;1m Tools Ini Di Tujukan Buat Para Jones Agar Cepat Mendapat Pacar :v"
+sleep 1
+echo " Di Harapkan Tidak Menykung Pacar Teman :v"
+sleep 2
+echo " "
+echo "\033[33;1m]=======================================>"
+sleep 1
+echo "\033[36;1mJTools Menyediakan :"
+sleep 1
+echo "\033[33;1m]=======================================>"
+
+echo "\033[36;1m1. Cara Deketin Cewek"
+sleep 1
+echo "\033[33;1m]=======================================>"
+sleep 1
+echo "\033[36;1m2. Cara Bikin Cewe Cinta Mati"
+sleep 1
+echo "\033[33;1m]=======================================>"
+sleep 1
+echo "\033[36;1m3. Cara Membuat Hubungan Awet Kayak Formalin"
+sleep 1
+echo "\033[33;1m]=======================================>"
+sleep 1
+echo "\033[36;1m4. Keluar Dari JTools"
+sleep 1
+echo "\033[33;1m]=======================================>"
+read -p "JTools@KikiLala:~#" command
+if [ $command -eq 1 ];
+	then
+clear
+figlet "Cara Deketin Cewek"
+sleep 2
+echo "\033[34;1m1. Lu Musti Akrab Ke Cewe Yang Lu Gebet.. Basa Basi Dan Becandaan Lah Biar Makin Akrab"
+sleep 5
+echo " "
+echo "\033[34;1m2. Lu Musti Bantu Dia Kalo Dia Ad Masalah Dan Kalo Dia Curhat Lu Dengerin Dan Kasih Saran Yang Bagus Biar Dia Terkesan Ke Lu "
+sleep 7
+echo " "
+echo "\033[34;1m3. Lu Musti Kasih Perhatian Lebih Ke Dia.. Misalnya Soal Makan Atau Tidur Dan Lain Lain"
+sleep 7
+echo " "
+echo "\033[34;1m4. Lu Musti Ad Klo Dia Butuh Lu , Lu Jngan Ngilang, Kalau Lu Off Seenggaknya Lu Ngasih Kabar Ke Dia. Jdi Dia Bakal Ngerasa Kalau Lu Slalu Ad Buat Dia Dan Dia Bakalan Nyaman Ke Lu"
+sleep 9
+sh JTools.sh
+fi
+
+if [ $command -eq 2 ];
+	then
+clear
+figlet " Cara Membuat Cewek Cinta Mati "
+sleep 2
+echo " "
+echo "\033[36;1m1. Kalau Dia Sudah Nyaman Ke Lu.. Otomatis Rasa Sayang Dan Cinta Bkalan Tumbuh.. Dan Perasaan Yang Timbul Dari Rasa Kenyamanan Sangat Besar Dan Sangat Tulus. Dan Ketika Si Cewe Udh Nyaman Buat Dia Butuh Lu."
+sleep 8
+echo " "
+echo "\033[36;1m2. Setelah SiCwek Butuh Lu,Otomatis Si Cwek Bakalan Cinta Mati Ke Lu"
+sleep 5
+fi
+
+if [ $command -eq 3 ];
+	then
+#!/system/xbin/bash
+clear
+figlet " Cara Membuat Hubungan Agar Langgeng"
+sleep 1
+echo " "
+echo "\033[31;1m1. Peliharalah rasa cinta yang seimbang, agar hubungan kalian senantiasa harmonis"
+sleep 4
+echo " "
+echo "\033[34;1mMenjadi dewasa berarti bisa menghargai kepentingan orang lain dan tidak memikirkan diri sendiri. Jangan merasa bangga jika kamu merasa pasanganmu lebih cinta, lebih memberi, lebih pengertian, atau lebih mengalah"
+sleep 12
+echo " "
+echo "\033[31;1m2. Saling Terbuka Satu Sama Lain"
+sleep 4
+echo " "
+echo "\033[34;1mSelesaikan semua masalah dengan terbuka. Berceritalah dengan pasanganmu dan carilah solusi bersama-sama. Jangan menutup diri dan berharap pasanganmu dapat mengerti keluh kesahmu dengan sendirinya. Berbicaralah dan diskusikan masalah bersama untuk memiliki hubungan yang lebih dewasa."
+sleep 13
+echo " "
+echo "\033[31;1m3. Jangan Membanding-Bandingkan Pasanganmu Dengan Orang Lain"
+sleep 4
+echo " "
+echo "\033[34;1mJangan mudah iri, jangan mudah membandingkan hubunganmu lebih indah atau lebih buruk daripada orang lain. Hal seperti ini akan menimbulkan rasa kecewa dan jenuh terhadap pasangan. Jika sudah berkomitmen, belajarlah untuk bisa menerima kondisi hubungan kalian dengan positif"
+sleep 15
+echo " "
+echo "\033[31;1m4. Jangan Menyerah Tapi Berjuang Bersama-Bersama"
+sleep 4
+echo " "
+echo "\033[34;1mHal yang terpenting dalam sebuah hubungan adalah keyakinan untuk berjuang bersama. Jika kamu dan pasangan sudah sama-sama yakin, jadikanlah keyakinan kalian sebagai motivasi untuk berjuang bersama. Jangan mudah menyerah dan dilahap oleh emosi ketika sedang bertengkar. Ingatlah hal-hal romantis kalian bersama ketika sedang marah. Ingatlah tujuan ke depan yang ingin kalian capai bersama. Dengan begitu, kamu dan pasangan bisa saling berjuang untuk meraih kebahagiaan kalian bersama"
+sleep 15
+echo " "
+echo "\033[31;1m5. Jangan Terlalu Posesif Terhadap Pasangan"
+sleep 4
+echo " "
+echo "\033[34;1mKarena Pasangan yang posesif punya kecenderungan untuk mengatur. Dia akan mengatur setiap detail persoalan hidup Pasangannya. Apa saja yang boleh dan tak boleh kamu lakukan, dia yang menentukan. Hal ini dapat membuat si cewek merasa terkekang dan bisa bisa hubungan kalian ga akan awet kayak formalin"
+sleep 13
+sh JTools.sh
+fi
+
+if [ $command -eq 4 ];
+	then
+	figlet "RoBoSkIjR_404  Lala Ceper"
+	sleep 2
+echo "\033[31;1m Sedang Keluar Dari JTools"
+sleep 2
+echo "\033[32;1m Terima Kasih Telah Menggunakan Tools Kami :)"
+sleep 1
+fi
